0160738ebb5d7c4db52f333a57ffb13a4b346eb7
mr.chen <chenhui315503490@126.com>
2017-10-19 09:17:50 +0800

    集成地图插件

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,5 +1,4 @@
 // page/map/map.js
-var amapFile = require('../../libs/map/amap-wx.js');//如：..­/..­/libs/amap-wx.js
 var markersData = [];
 Page({
   data: {
@@ -15,42 +14,95 @@ Page({
     that.changeMarkerColor(markersData, id);
   },
   onLoad: function () {
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+        that.setData({
+          latitude: latitude
+        });
+        that.setData({
+          longitude: longitude
+        });
+        console.log(res);
+      }
+      
+    })
     var that = this;
-    var myAmapFun = new amapFile.AMapWX({ key: '8c52e30969e901461b1796ae95525543' });
-    myAmapFun.getPoiAround({
-      iconPathSelected: '../../images/map/marker.png', //如/：..­/..­/img/marker_checked.png
-      iconPath: '../../images/map/marker.png', //如：..­/..­/img/marker.png
-      success: function (data) {
-        markersData = data.markers;
-        that.setData({
-          markers: markersData
-        });
-        that.setData({
-          latitude: markersData[0].latitude
-        });
-        that.setData({
-          longitude: markersData[0].longitude
-        });
-        console.log(markersData)
-        that.showMarkerInfo(markersData, 0);
+    
+    wx.request({
+      url: 'http://192.168.1.102:1314/api/v1/gymHourse/list',
+      method:'GET',
+      header: {
+        'content-type': 'application/json',
+        'code':'123'
       },
-      fail: function (info) {
-        wx.showModal({ title: info.errMsg })
+      success: function (res) {
+        markersData = res.data;
+      
+        var _markers = []
+        for (var i = 0; i < res.data.length; i++) {
+          var latitude = Number(res.data[i].latitude);
+          var longitude = Number(res.data[i].longitude);
+          var id = Number(res.data[i].id);
+          var marker = {
+            latitude: latitude,
+            longitude: longitude,
+            iconPath: "../../images/map/marker.png",
+            id:id,
+            width:50,
+            height:50
+          }
+          _markers.push(marker)
+        }
+        console.log(_markers)
+        that.setData({
+          markers: _markers
+        });
+      },
+      fail:function (res){
+        console.log(res)
       }
     })
+    // var myAmapFun = new amapFile.AMapWX({ key: '8c52e30969e901461b1796ae95525543' });
+    // myAmapFun.getPoiAround({
+    //   iconPathSelected: '../../images/map/marker.png', //如/：..­/..­/img/marker_checked.png
+    //   iconPath: '../../images/map/marker.png', //如：..­/..­/img/marker.png
+    //   success: function (data) {
+    //     markersData = data.markers;
+    //     that.setData({
+    //       markers: markersData
+    //     });
+    //     that.setData({
+    //       latitude: markersData[0].latitude
+    //     });
+    //     that.setData({
+    //       longitude: markersData[0].longitude
+    //     });
+    //     console.log(markersData)
+    //     that.showMarkerInfo(markersData, 0);
+    //   },
+    //   fail: function (info) {
+    //     wx.showModal({ title: info.errMsg })
+    //   }
+    // })
   },
   showMarkerInfo: function (data, i) {
     var that = this;
     that.setData({
       textData: {
         name: data[i].name,
-        desc: data[i].address
+        desc: data[i].gymItems
       }
     });
   },
   changeMarkerColor: function (data, i) {
     var that = this;
     var markers = [];
+    console.log(data);
     for (var j = 0; j < data.length; j++) {
       if (j == i) {
         data[j].iconPath = "../../images/map/marker.png"; //如：..­/..­/img/marker_checked.png
