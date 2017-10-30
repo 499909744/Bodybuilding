e7478b0ce3238977bf9f4672d84faf8c5d249110
yn <1065800184@qq.com>
2017-10-30 18:55:30 +0800

    倒计时

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,69 +5,88 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    timer: '',
+    second: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
-  },
 
+  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
 
-  linkMap :function(){
+  linkMap: function () {
     wx.navigateTo({
       url: '../map/map'
     })
+  },
+  startCountdown: function () {
+
+    this.setData({
+      second: 60
+    })
+    let second = this.second = 60;
+    clearInterval(this.timer);
+    this.timer = setInterval(() => {
+      if (second > 0) {
+        second--;
+        this.setData({
+          second: second
+        })
+      } else {
+        clearInterval(this.timer);
+      }
+    }, 1000)
   }
+
 })
